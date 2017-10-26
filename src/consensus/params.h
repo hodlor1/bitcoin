5edf781115479524b5e756cdf13f132c9ab766c0
vis <visbtc@bitbucket.org>
2017-10-27 00:50:03 +0300

    add different graph size for testnet

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -57,6 +57,7 @@ struct Params {
     /** Proof of work parameters */
     uint256 powLimit;
     uint256 cuckooPowLimit;
+	uint32_t cuckooGraphSize;
     bool fPowAllowMinDifficultyBlocks;
     bool fPowNoRetargeting;
     int64_t nPowTargetSpacing;
